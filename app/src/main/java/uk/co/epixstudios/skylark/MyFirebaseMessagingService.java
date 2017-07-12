3fe201305c6660cecbf5d2bf382f6e1d5030fa5e
Damian Moore <damian@epixstudios.co.uk>
2017-07-12 21:54:44 +0100

    Data message type, display large icon from URL, styling

--- app/src/main/java/uk/co/epixstudios/skylark/MyFirebaseMessagingService.java
+++ app/src/main/java/uk/co/epixstudios/skylark/MyFirebaseMessagingService.java
@@ -4,35 +4,62 @@ import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Color;
 import android.media.RingtoneManager;
 import android.net.Uri;
-import android.support.v4.app.NotificationCompat;
+import android.support.v7.app.NotificationCompat;
+import android.support.v4.content.ContextCompat;
 
 import com.google.firebase.messaging.FirebaseMessagingService;
 import com.google.firebase.messaging.RemoteMessage;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+import java.util.Map;
+
 
 public class MyFirebaseMessagingService extends FirebaseMessagingService {
-
     @Override
     public void onMessageReceived(RemoteMessage remoteMessage) {
-        sendNotification(remoteMessage.getNotification().getBody());
+        sendNotification(remoteMessage.getData());
     }
 
-    private void sendNotification(String messageBody) {
+    private void sendNotification(Map<String, String> data) {
         Intent intent = new Intent(this, MainActivity.class);
         intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
         PendingIntent pendingIntent = PendingIntent.getActivity(this, 0 /* Request code */, intent,
                 PendingIntent.FLAG_ONE_SHOT);
 
-        Uri defaultSoundUri= RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
-        NotificationCompat.Builder notificationBuilder = new NotificationCompat.Builder(this)
-                .setSmallIcon(R.mipmap.ic_notification)
-                .setContentTitle("Message received")
-                .setContentText(messageBody)
+        Uri defaultSoundUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
+
+        Bitmap remotePicture = BitmapFactory.decodeResource(getResources(), R.mipmap.ic_launcher);
+        try {
+            remotePicture = BitmapFactory.decodeStream((InputStream) new URL(data.get("icon")).getContent());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        int myColor = ContextCompat.getColor(getApplicationContext(), R.color.colorPrimary);
+        try {
+            myColor = Color.parseColor(data.get("color"));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        NotificationCompat.Builder notificationBuilder = new NotificationCompat.Builder(this);
+        notificationBuilder.setSmallIcon(R.mipmap.ic_notification)
+                .setLargeIcon(remotePicture)
+                .setContentTitle(data.get("title"))
+                .setContentText(data.get("body"))
                 .setAutoCancel(true)
                 .setSound(defaultSoundUri)
-                .setContentIntent(pendingIntent);
+                .setColor(myColor)
+                .setContentIntent(pendingIntent)
+                .setDefaults(NotificationCompat.DEFAULT_ALL)
+                .setPriority(NotificationCompat.PRIORITY_HIGH);
 
         NotificationManager notificationManager =
                 (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
