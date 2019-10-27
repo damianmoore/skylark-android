c712855b29f3b3771f42fca721ca390a179523a5
Damian Moore <damian@epixstudios.co.uk>
2019-10-27 22:24:08 +0000

    Back button key controls webview

--- app/src/main/java/uk/co/epixstudios/skylark/WebviewActivity.java
+++ app/src/main/java/uk/co/epixstudios/skylark/WebviewActivity.java
@@ -1,6 +1,7 @@
 package uk.co.epixstudios.skylark;
 
 import android.os.Bundle;
+import android.view.KeyEvent;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 
@@ -8,17 +9,37 @@ import androidx.appcompat.app.AppCompatActivity;
 
 
 public class WebviewActivity extends AppCompatActivity {
+
+    WebView wv = null;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_webview);
 
-        WebView wv = (WebView) findViewById(R.id.webview);
+        wv = (WebView) findViewById(R.id.webview);
         wv.setWebViewClient(new WebViewClient());
         wv.clearHistory();
         wv.getSettings().setJavaScriptEnabled(true);
 
         String url = getIntent().getStringExtra("URL");
         wv.loadUrl(url);
+    }
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        if (event.getAction() == KeyEvent.ACTION_DOWN) {
+            switch (keyCode) {
+                case KeyEvent.KEYCODE_BACK:
+                    if (wv.canGoBack()) {
+                        wv.goBack();
+                    } else {
+                        finish();
+                    }
+                    return true;
+            }
+
+        }
+        return super.onKeyDown(keyCode, event);
     }
 }
